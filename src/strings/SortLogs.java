8474d19758dd345c15a7e80fe28ae22de2ad3244
sunjin <kangjoshi@nhn.com>
2024-05-14 14:09:45 +0900

    SortLogs

--- src/strings/SortLogs.java
+++ src/strings/SortLogs.java
@@ -1,5 +1,9 @@
 package strings;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * 로그 정렬
  * - 로그를 정렬하라
@@ -14,15 +18,80 @@ public class SortLogs {
 
     public static void main(String[] args) {
 
-        var inputs = new Stirng
+        var inputs = new String[]{
+                "id1 8 1 5 1",
+                "id2 art can",
+                "id3 3 6",
+                "id4 own kit dig",
+                "id5 art zero"
+        };
 
+        var sortedLogs = sort(inputs);
 
+        for (var log : sortedLogs) {
+            System.out.println(log);
+        }
     }
 
     /**
      * 시도
-     * -
+     * - 문자, 숫자로 된 로그 분리
+     * - 문자로 된 로그 비교하며 정렬
+     * - 숫자로 된 로그는 순서 유지
+     * - 문자 로그 + 숫자 로그
      * 풀이
      * -
      * */
+    public static String[] sort(String[] logs) {
+
+        var needSortLogs = new ArrayList<String>();
+        var noNeedSortLogs = new ArrayList<String>();
+
+        for (var log : logs) {
+            if (isLetterLog(log)) {
+                needSortLogs.add(log);
+            } else {
+                noNeedSortLogs.add(log);
+            }
+        }
+
+        sortLog(needSortLogs);
+
+        needSortLogs.addAll(noNeedSortLogs);
+        return needSortLogs.toArray(new String[needSortLogs.size()]);
+    }
+
+    public static List<String> sortLog(List<String> logs) {
+
+        logs.sort((log1, log2) -> {
+            var logContents1 = splitLog(log1);
+            var logContents2 = splitLog(log2);
+
+            if (log1.substring(logContents1[0].length()).equals(log2.substring(logContents2[0].length()))) {
+                return logContents1[0].compareTo(logContents2[0]);
+            } else {
+                return log1.substring(logContents1[0].length()).compareTo(log2.substring(logContents2[0].length()));
+            }
+        });
+
+        return logs;
+    }
+
+
+    public static boolean isLetterLog(String log) {
+        if (log == null) {
+            return false;
+        }
+
+        var contents = splitLog(log);
+        if (contents.length < 2) {
+            return false;
+        }
+
+        return Character.isLetter(contents[1].charAt(0));
+    }
+
+    public static String[] splitLog(String log) {
+        return log.split(" ");
+    }
 }
