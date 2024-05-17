98eac06720c11a397eb5a082c5dea790b09f8f7e
sunjin <kangjoshi@nhn.com>
2024-05-17 17:55:53 +0900

    SwapNodesInPairs, OddEvenLinkedList 풀이

--- src/linked_list/OddEvenLinkedList.java
+++ src/linked_list/OddEvenLinkedList.java
@@ -1,5 +1,11 @@
 package linked_list;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * 홀짝 연결리스트
  * - 연결 리스트를 홀수 번째 노드 다음에 짝수 번째 노드가 오도록 재구성하라. 공간 복잡도 O(1), 시간 복잡도 O(n)에 풀이하라
@@ -10,17 +16,40 @@ package linked_list;
 public class OddEvenLinkedList {
 
     public static void main(String[] args) {
-
         // 1 -> 2 -> 3 -> 4 -> 5 -> 6
         // 1 -> 3 -> 5 -> 2 -> 4 -> 6
+        var numbers = new LinkedList<>(List.of(1, 2, 3, 4, 5, 6));
 
+        var result = oddEven(numbers);
 
+        result.forEach(System.out::print);
     }
 
     /**
      * 시도
-     * -
+     * - 홀짝 판단 %2하여 나머지 여부로 확인
+     * - 공간 복잡도 O(1)은 신규 배열 생성 없이, 시간 복잡도 O(n)은 중첩된 for문 없이 해결.
      * 풀이
      * -
      * */
+    public static List<Integer> oddEven(LinkedList<Integer> numbers) {
+        // 일단 공간 복잡도는 생각하지 말고 해보자..
+        var result = new LinkedList<Integer>(); // 이 부분이 문제가 될 것 같음?
+
+        var oddIndex = 0;
+        while (!numbers.isEmpty()) {
+            var number = numbers.poll();
+            if (isOdd(number)) {
+                result.add(oddIndex, number);
+                oddIndex++;
+            } else {
+                result.addLast(number);
+            }
+        }
+        return result;
+    }
+
+    public static boolean isOdd(int number) {
+        return number % 2 == 1;
+    }
 }
--- src/linked_list/SwapNodesInPairs.java
+++ src/linked_list/SwapNodesInPairs.java
@@ -1,5 +1,8 @@
 package linked_list;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 페어의 노드 스왑
  * - 연결 리스트를 입력 받아 페어 단위로 스왑하라
@@ -10,16 +13,27 @@ package linked_list;
 public class SwapNodesInPairs {
 
     public static void main(String[] args) {
-
         // 1 -> 2 -> 3 -> 4 -> 5 -> 6
         // 2 -> 1 -> 4 -> 3 -> 6 -> 5
+        var list = new LinkedList<>(List.of(1, 2, 3, 4, 5, 6));
 
+        swap(list);
+
+        list.forEach(System.out::print);
     }
 
     /**
      * 시도
-     * -
+     * - +2씩 돌면서 i <-> i - 1
      * 풀이
      * -
      * */
+    public static void swap(List<Integer> list) {
+        Integer temp;
+        for (int i = 0; i < list.size(); i += 2) {
+            temp = list.get(i);
+            list.set(i, list.get(i + 1));
+            list.set(i + 1, temp);
+        }
+    }
 }
