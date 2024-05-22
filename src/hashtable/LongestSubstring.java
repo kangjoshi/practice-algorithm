f467e4b193e11d641ae009951da6b51a84e5e7d5
sunjin <kangjoshi@nhn.com>
2024-05-22 17:26:39 +0900

    LongestSubstring 풀이

--- src/hashtable/LongestSubstring.java
+++ src/hashtable/LongestSubstring.java
@@ -1,5 +1,8 @@
 package hashtable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 중복 문자 없는 가장 긴 부분 문자열
  * - 중복 문자가 없는 가장 긴 부분 문자열의 길이를 리턴
@@ -10,12 +13,27 @@ package hashtable;
 public class LongestSubstring {
 
     public static void main(String[] args) {
-        var input = "abcabcbbc"; // 3
+        var input = "abcabcbbc"; // abc 3
+
+        var charArray = input.toCharArray();
+        var set = new HashSet<Character>(charArray.length);
+
+        var maxLength = Integer.MIN_VALUE;
+        for (int i = 0; i < charArray.length; i++) {
+
+            if (set.contains(charArray[i])) {
+                maxLength = Math.max(set.size(), maxLength);
+                set.clear();
+            }
+            set.add(charArray[i]);
+        }
+        maxLength = Math.max(maxLength, set.size());
+        System.out.println(maxLength);
     }
 
     /**
      * 시도
-     * -
+     * - 순회하면서 한글자씩 set에 입력, set에 존재하는 문자열을 만나면 해당 사이즈를 길이로 기록, 사이즈를 비교하며 가장 큰 길이를 반환
      * 풀이
      * -
      * */
