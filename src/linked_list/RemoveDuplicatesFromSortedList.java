5e3d50d6bdc54d35343bcc37bc8e93c84a4bde58
sunjin <kangjoshi@nhn.com>
2024-05-31 11:48:21 +0900

    RemoveDuplicatesFromSortedList 풀이

--- src/linked_list/RemoveDuplicatesFromSortedList.java
+++ src/linked_list/RemoveDuplicatesFromSortedList.java
@@ -12,22 +12,37 @@ package linked_list;
 public class RemoveDuplicatesFromSortedList {
 
     public static void main(String[] args) {
-
         // 1 -> 1 -> 2 => 1 -> 2
         // 1 -> 1-> 2 -> 3 -> 3 => 1 -> 2 -> 3
+        var node = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(2,  new ListNode(3, new ListNode(3, new ListNode(4, new ListNode(99))))))));
+        var removed = removeDuplicates(node);
 
-        var node = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
-        removeDuplicates(node);
+        removed.printAll();
     }
 
     /**
      * 시도
+     * 1차 생각
      * - 순회하면서 임시 변수에 값 할당
      * - 정렬 되어 있다? -> 현재 노드의 값이 임시 변수의 값보다 작다면 현재 노드의 값의 중복 값은 없음.
+     * 2차 생각
+     * - left, right 비교 하면서 같다면 right를 다음 값으로
      * 풀이
      * -
      * */
-    public static void removeDuplicates(ListNode head) {
-
+    public static ListNode removeDuplicates(ListNode head) {
+        var left = head;
+        var right = head.next;
+        while (right != null) {
+            // System.out.println(left.val + " : " + right.val);
+            if (left.val == right.val) {
+                right = right.next;
+                left.next = right;
+            } else {
+                left = left.next;
+                right = right.next;
+            }
+        }
+        return head;
     }
 }
