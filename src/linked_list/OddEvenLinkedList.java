fdf4d8df516aeee1368dc570e5d325993a543daa
ShinBoReum <tlsqhfma@naver.com>
2024-06-23 16:40:15 +0900

    OddEvenLinkedList 풀이, 회고

--- src/linked_list/OddEvenLinkedList.java
+++ src/linked_list/OddEvenLinkedList.java
@@ -19,7 +19,8 @@ public class OddEvenLinkedList {
         // 1 -> 2 -> 3 -> 4 -> 5 -> 6
         // 1 -> 3 -> 5 -> 2 -> 4 -> 6
 
-        var node = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+        var node = new ListNode(2, new ListNode(1, new ListNode(3, new ListNode(5, new ListNode(6, new ListNode(4))))));
+
         var result = oddEven(node);
 
         System.out.println();
@@ -30,30 +31,30 @@ public class OddEvenLinkedList {
      * - 비교하면서 앞수가 짝수면서 뒷수가 홀수라면 뒤집기
      * 풀이
      * -
+     * 회고
+     * - 문제를 완전하게 잘 못 이해 했다.. 값이 짝수 홀수 인지에 따라 배치는 것인줄..
+     * - 입력과 출력을 보고 문제를 이해 하는 습관보다는 지문을 잘 읽는 습관을 들여야겠다.
      * */
     public static ListNode oddEven(ListNode head) {
-        var dummy = new ListNode(0, head);
 
-        var current = dummy.next;
-        while (current.next != null) {
-            var left = current;
-            var right = current.next;
-
-            if (isEven(left) && isOdd(right)) {
-                current = right;
-                current.next = left;
-            }
-
-            current = current.next;
+        if (head == null) {
+            return head;
         }
 
+        var odd = head;
+        var even = head.next;
 
-        return dummy.next;
+        var evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return head;
     }
-
-    public static boolean isOdd(ListNode node) {
-        return node.val % 2 == 1;
-    }
-
-    public static boolean isEven(ListNode node) { return !isOdd(node); }
 }
